2830eb01be8167bbf0f9d6c9a831c4cd40a7b967
Juanfro <juanfropro@gmail.com>
2018-05-05 22:25:30 +0200

    Arreglado Listar Autores

--- application/controllers/Autor.php
+++ application/controllers/Autor.php
@@ -1,68 +1,58 @@
 <?php
-class Autor extends CI_Controller{
-	public function index()
-	{   $this->crear();
-	
+
+class Autor extends CI_Controller {
+
+	public function index() {
+		$this->crear();
 	}
-	public function crear(){
-		enmarcar($this,'Autores/crear');
-	
+
+	public function crear() {
+		enmarcar($this, 'autor/crear');
 	}
-	public function crearpost(){
-		$nombre=$_POST['nombre'];
-		$pseudonimo=$_POST['pseudonimo'];
-		$this->load->model('autores_model');
-		$this->autores_model->crear($nombre,$pseudonimo);
-		enmarcar($this,'Autores/crearOK' );
+
+	public function crearpost() {
+		$nombre = $_POST['nombre'];
+		$pseudonimo = $_POST['pseudonimo'];
+		$this->load->model('autor_model');
+		$this->autor_model->crear($nombre, $pseudonimo);
+		enmarcar($this, 'autor/crearOK');
 	}
-	
-	public function modificar(){
+
+	public function modificar() {
+		$this->load->model('autor_model');
+		$id_autor = $_POST['id_autor'];
+		$datos['autor'] = $this->autor_model->getautorPorId($id_autor);
 		
-		
-		$this->load->model('autores_model');
-		$id_autores=$_POST['id_autores'];
-	    $datos['autores']= $this->autores_model->getAutoresPorId($id_autores);
-	
-		enmarcar($this, 'Autores/modificar',$datos);
-		
+		enmarcar($this, 'autor/modificar', $datos);
 	}
-	
-	public function modificarpost(){
+
+	public function modificarpost() {
+		$nombre = $_POST['nombre'];
+		$pseudonimo = $_POST['pseudonimo'];
 		
+		$id_autor = $_POST['id_autor'];
 		
-		$nombre=$_POST['nombre'];
-		$pseudonimo=$_POST['pseudonimo'];
-		
-		$id_autores=$_POST['id_autores'];
-		
-		$this->load->model('autores_model');
-		$this->autores_model->modificar($id_autores,$nombre,$pseudonimo);
-		
-		
-		
+		$this->load->model('autor_model');
+		$this->autor_model->modificar($id_autor, $nombre, $pseudonimo);
 	}
-	
-	public  function borrar (){
-		
-		
-		$id_autores =$_POST['id_autores'];
-		$this->load->model('autores_model');
-		$this->autores_model->borrar($id_autores);
+
+	public function borrar() {
+		$id_autor = $_POST['id_autor'];
+		$this->load->model('autor_model');
+		$this->autor_model->borrar($id_autor);
 		$this->listar();
-		
 	}
-	
-		
-	
+
 	public function listar() {
-		$this->listarPost ();
+		$this->listarPost();
 	}
+
 	public function listarPost($f = '') {
-		$filtro = isset ( $_POST ['filtro'] ) ? $_POST ['filtro'] : $f;
-		$this->load->model('autores_model');
-		$datos['autores']= $this->autores_model->getAll($filtro);
-		$datos['filtro'] =$filtro;
-		enmarcar($this, 'Autores/listar', $datos);
+		$filtro = isset($_POST['filtro']) ? $_POST['filtro'] : $f;
+		$this->load->model('autor_model');
+		$datos['autores'] = $this->autor_model->getAll($filtro);
+		$datos['filtro'] = $filtro;
+		enmarcar($this, 'autor/listar', $datos);
 	}
 }
 ?>
