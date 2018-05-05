93a5e0dd26dae678ee4a432af84f4073f778d563
Juanfro <juanfropro@gmail.com>
2018-05-05 22:26:13 +0200

    Arreglado Listar Autores

---
+++ application/models/autor_model.php
@@ -0,0 +1,38 @@
+<?php
+
+class autor_model extends CI_model {
+
+	public function crear($nombre, $pseudonimo) {
+		$autor = R::dispense('autor');
+		$autor->nombre = $nombre;
+		$autor->pseudonimo = $pseudonimo;
+		R::store($autor);
+		R::close();
+	}
+
+	public function getAll($filtro) {
+		return R::find('autor', "nombre like ?", [
+			'%' . $filtro . '%'
+		]);
+	}
+
+	public function getautorPorId($id_autor) {
+		return R::load('autor', $id_autor);
+	}
+
+	public function modificar($id_autor, $nombre, $pseudonimo) {
+		$autor = R::load('autor', $id_autor);
+		$autor->nombre = $nombre;
+		$autor->pseudonimo = $pseudonimo;
+		R::store($autor);
+	}
+
+	public function borrar($id_autor) {
+		$autor = R::load("autor", $id_autor);
+		if ($autor->id != 0) {
+			R::trash($autor);
+		}
+		R::close();
+	}
+}
+?>
