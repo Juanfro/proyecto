0ca48dbf3af5f911ea41001a2f1a832ca5a255ef
Juanfro <juanfropro@gmail.com>
2018-05-19 18:34:24 +0200

    Añadida selección de Rol al crear Usuario

--- application/views/Usuario/crear.php
+++ application/views/Usuario/crear.php
@@ -4,29 +4,41 @@
 			<legend>
 				<label for="idusuario"> Usuario</label>
 			</legend>
-			<label for="nombre">Nombre</label> <input type="text" name="nombre"
-				id="nombre"></input><br><br>
-				
-		    <label for="apellido">Apellido</label> <input type="text" name="apellido"
-				id="apellido"></input><br><br>
-				
-			<label for="alias">Alias</label> <input type="text" name="alias"
-				id="alias" required="required"></input><br><br>
-			
-			<label for="contrasena">Contraseña</label> <input type="text" name="contrasena"
-				id="contrasena" required="required"></input><br><br>
-				
-			<label for="rol">Rol</label> <input type="text" name="rol"
-				id="rol" required="required"></input><br><br><!-- La etiqueta required Sólo debe aparecer en la vista del administrador -->
-				
-			<label for="email">Email</label> <input type="text" name="email"
-				id="email" required="required"></input><br><br>
-			
-			<label for="edad">Edad</label> <input type="text" name="edad"
-				id="edad"></input><br><br>			
-			
-			<input type="submit" value="registrar usuario">
+			<label for="nombre">Nombre</label>
+			<input type="text" name="nombre" id="nombre"></input><br>
+			<br>
+
+			<label for="apellido">Apellido</label>
+			<input type="text" name="apellido" id="apellido"></input><br>
+			<br>
+
+			<label for="alias">Alias</label>
+			<input type="text" name="alias" id="alias" required="required"></input><br>
+			<br>
+
+			<label for="contrasena">Contraseña</label>
+			<input type="text" name="contrasena" id="contrasena" required="required"></input><br>
+			<br>
+
+			<label for="rol">Rol</label>
+			<!-- <input type="text" name="rol" id="rol" required="required"></input> -->
+			<select name="rol" id="rol">
+				<option value="usuario">Usuario</option>
+				<option value="editor">editor</option>
+				<option value="administrador">Administrador</option>				
+			</select>
+			<br>
+			<br>
+			<!-- La etiqueta required Sólo debe aparecer en la vista del administrador -->
+
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" required="required"></input><br>
+			<br>
+
+			<label for="edad">Edad</label>
+			<input type="text" name="edad" id="edad"></input><br>
+			<br> <input type="submit" value="registrar usuario">
 		</fieldset>
 	</form>
-	
+
 </div>
