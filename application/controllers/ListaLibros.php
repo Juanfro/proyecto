7f5541027b97fceb25f1c10d6df925c7cf6973ca
adriana <usuario@Adriana>
2018-05-23 13:54:20 +0200

    Revert "crea lista de libros"
    This reverts commit 74ff459f43168a8334c99c1a31346cdd7d859435.

--- application/controllers/ListaLibros.php
+++ application/controllers/ListaLibros.php
@@ -5,26 +5,8 @@ class ListaLibros extends CI_Controller{
 	
 	}
 	public function crear(){
-		$this->load->model('libro_model');
-		
-		$datos['libros']=$this->libro_model->getAll();
-		enmarcar($this,'ListaLibros/crear',$datos);
+		enmarcar($this,'ListaLibros/crear');
 
-	}
-	public  function crearPost(){
-		$listalibros = R::dispense('listalibros');
-		$libros=$_POST['libros'];
-		$nombre=$_POST['nombre'];
-		$listalibros->nombre=$nombre;
-		foreach ($libros as $libro){
-				
-			$listalibros->listautores=$libro;
-		}
-		
-		
-		
-		R::store($listalibros);
-		R::close();
 	}
 }
 ?>
