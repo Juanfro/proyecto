951e85e867bc51fe3927f30123d5cb5c9b08b1c8
Juanfro <juanfropro@gmail.com>
2018-06-02 22:05:01 +0200

    Modificado Input de nota En Valoraciñon

--- application/views/Valoracion/crear.php
+++ application/views/Valoracion/crear.php
@@ -8,7 +8,20 @@
 			<input type="hidden" name="id_libro" value="<?= $id_libro?>"></input>
 			
 			<label for="nota">nota</label>
-		 	<input type="text" name="nota" id="nota" ></input><br><br>
+			
+			<select name="nota">
+				<option value="1" label="1">			
+				<option value="2" label="2">			
+				<option value="3" label="3">			
+				<option value="4" label="4">			
+				<option value="5" label="5">			
+				<option value="6" label="6">			
+				<option value="7" label="7">			
+				<option value="8" label="8">			
+				<option value="9" label="9">			
+				<option value="10" label="10">							
+			</select>			
+		 	<br><br>
 				
 			<label for="contenido">Contenido</label> 
 			<textarea name="contenido" id="contenido" ></textarea><br><br>				
