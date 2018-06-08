e69ab7f0a5438cb6f358a8a8de1846e69c5b5003
Juanfro <juanfropro@gmail.com>
2018-06-08 23:21:58 +0200

    Guest No puede seguir Autor. usuario no puede crear/modificar Autor

--- application/views/Autor/listar.php
+++ application/views/Autor/listar.php
@@ -10,11 +10,14 @@
 		<tr>
 			<th>Nombre</th>
 			<th>Pseudonimo</th>
-			<th>Pais</th>
-			<th>Modificar</th>
-			<th>Eliminar</th>
-			<th>Listar</th>
-			
+			<th>Pais</th>		
+			<?php if(isset ($_SESSION['usuario']) && ($_SESSION['usuario']['rol'] == 'administrador' || $_SESSION['usuario']['rol'] == 'editor')):?>
+				<th>Modificar</th>
+				<th>Eliminar</th>
+			<?php endif;?>
+			<?php if(isset($_SESSION['usuario'])):?>
+				<th>Seguir</th>
+			<?php endif;?>
 		</tr>
 	</thead>
 	<tbody>
@@ -27,25 +30,31 @@
 			</td>
 			<td class="jcorgFilterTextChild"><?=$autor->pseudonimo ?></td>
 			<td><?= $autor->pais->nombre ?></td>
-			<td>
-				<form action="<?=base_url()?>Autor/modificar" method="post">
-					<input type="hidden" name="id_autor" id="id_autor" value="<?=$autor->id?>" /> 
-					<input type="hidden" name="filtro" value="<?=$filtro ?>" />
-					<button class=" glyphicon glyphicon-edit" type="submit"></button>
-				</form>
-			</td>
-			<td>
-				<form action="<?=base_url()?>Autor/borrar" method="post">
-					<input type="hidden" name="id_autor" value="<?=$autor->id?>" />
-					<button class="glyphicon glyphicon-remove" type="submit"></button>
-				</form>
-			</td>
-			<td>
-				<form action="<?= base_url()?>Autor/seguir" method="post">
-					<input type="hidden" name="id_autor" id="id_autor" value="<?= $autor->id?>"/>
-					<button class="glyphicon glyphicon-heart" type="submit"></button>
-				</form>
-			</td>
+			
+			<?php if(isset ($_SESSION['usuario']) && ($_SESSION['usuario']['rol'] == 'administrador' || $_SESSION['usuario']['rol'] == 'editor')):?>
+				<td>
+					<form action="<?=base_url()?>Autor/modificar" method="post">
+						<input type="hidden" name="id_autor" id="id_autor" value="<?=$autor->id?>" /> 
+						<input type="hidden" name="filtro" value="<?=$filtro ?>" />
+						<button class=" glyphicon glyphicon-edit" type="submit"></button>
+					</form>
+				</td>
+				<td>
+					<form action="<?=base_url()?>Autor/borrar" method="post">
+						<input type="hidden" name="id_autor" value="<?=$autor->id?>" />
+						<button class="glyphicon glyphicon-remove" type="submit"></button>
+					</form>
+				</td>
+			<?php endif;?>
+			
+			<?php if(isset($_SESSION['usuario']) ): ?>
+				<td>
+					<form action="<?= base_url()?>Autor/seguir" method="post">
+						<input type="hidden" name="id_autor" id="id_autor" value="<?= $autor->id?>"/>
+						<button class="glyphicon glyphicon-heart" type="submit"></button>
+					</form>
+				</td>
+			<?php endif;?>
 			
 		</tr>
 			 
