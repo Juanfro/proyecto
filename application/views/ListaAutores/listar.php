6db161c9b0ae64fbf47391b406115c9302b8c5c5
Juanfro <juanfropro@gmail.com>
2018-06-09 21:37:58 +0200

    ListaLibros. Guest No puede Seguir Lista

--- application/views/ListaAutores/listar.php
+++ application/views/ListaAutores/listar.php
@@ -19,7 +19,10 @@
 				<th>Autor de la lista</th>
 				<th>Editar</th>
 				<th>Borrar</th>
-				<th>Seguir</th>
+				
+				<?php if(isset($_SESSION['usuario']) ):?>
+					<th>Seguir</th>
+				<?php endif;?>				
 								
 			</tr>
 		</thead>
@@ -27,45 +30,49 @@
 			<?php foreach ($listas as $lista):?>
 				<tr>
 					<!-- Nombre -->
-					<td>
-						<a href="<?= base_url()?>ListaAutores/perfil/<?=$lista->id ?>" class="jcorgFilterTextChild">
-							<?= $lista->nombre?>
-						</a>
-					</td>
-					
+					<td><a href="<?= base_url()?>ListaAutores/perfil/<?=$lista->id ?>" class="jcorgFilterTextChild">
+								<?= $lista->nombre?>
+							</a></td>
+	
 					<!-- Autores -->
 					<td>
-					<?php foreach($lista->sharedAutorList as $autor): ?>
-					 <?= $autor->nombre ."</br> "; ?> 
-					<?php endforeach; ?>
-					</td>
-					
+						<?php foreach($lista->sharedAutorList as $autor): ?>
+						 <?= $autor->nombre ."</br> "; ?> 
+						<?php endforeach; ?>
+						</td>
+	
 					<!-- Creador de la Lista -->
 					<td>
-						<?= $lista->usuario->alias?>
-					</td>
-					
+							<?= $lista->usuario->alias?>
+						</td>
+	
+					<!-- Modificar -->
 					<td>
-					  <form action="<?=base_url()?>ListaAutores/modificar" method="post">
-						<input type="hidden" name="id_lista" id="id_lista" value="<?=$lista->id?>" /> 
-						<input type="hidden" name="filtro" value="<?=$filtro ?>" />
-						<button class=" glyphicon glyphicon-edit" type="submit"></button>
-					  </form>
+						<form action="<?=base_url()?>ListaAutores/modificar" method="post">
+							<input type="hidden" name="id_lista" id="id_lista" value="<?=$lista->id?>" /> <input
+								type="hidden" name="filtro" value="<?=$filtro ?>" />
+							<button class=" glyphicon glyphicon-edit" type="submit"></button>
+						</form>
 					</td>
-					
+	
+					<!-- Borrar -->
 					<td>
-					  <form action="<?=base_url()?>ListaAutores/borrar" method="post">
-						<input type="hidden" name="id_lista"  id="id_lista" value="<?=$lista->id?>" />
-						<button class="glyphicon glyphicon-remove" type="submit"></button>
-					  </form>
+						<form action="<?=base_url()?>ListaAutores/borrar" method="post">
+							<input type="hidden" name="id_lista" id="id_lista" value="<?=$lista->id?>" />
+							<button class="glyphicon glyphicon-remove" type="submit"></button>
+						</form>
 					</td>
+	
 					
-					<td>
-					  <form action="<?= base_url()?>ListaAutores/seguir" method="post">
-						<input type="hidden" name="id_lista" id="id_lista" value="<?= $lista->id?>"/>
-						<button class="glyphicon glyphicon-heart" type="submit"></button>
-					  </form>
-					</td>
+					<!-- Seguir -->
+					<?php if(isset($_SESSION['usuario']) ):?>
+						<td>
+							<form action="<?= base_url()?>ListaAutores/seguir" method="post">
+								<input type="hidden" name="id_lista" id="id_lista" value="<?= $lista->id?>" />
+								<button class="glyphicon glyphicon-heart" type="submit"></button>
+							</form>
+						</td>
+					<?php endif;?>
 					
 				</tr>
 			<?php endforeach;?>
