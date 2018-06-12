cc453f58f723aee093b98d26dca87f50b5b64f16
Ethan2001 <usuario@Adriana>
2018-06-12 21:55:12 +0200

    modificar articulo  arreglado

--- application/views/Articulo/modificar.php
+++ application/views/Articulo/modificar.php
@@ -6,13 +6,13 @@
 			</legend>
 			
 			<label for="titulo">Titulo</label>
-		 	<input type="text" name="titulo" id="titulo" placeholder="<?=$articulo->titulo ?>"></input><br><br>
+		 	<input type="text" name="titulo" id="titulo" value="<?=$articulo->titulo ?>"></input><br><br>
 				
 			<label for="contenido">Contenido</label> 
 			<textarea name="contenido" id="contenido" placeholder="<?=$articulo->contenido ?>"></textarea><br><br>				
 			
 			
-			<input type="hidden" name="id_autores" value="<?= $articulo->id?>">
+			<input type="hidden" name="id_articulo" value="<?= $articulo->id?>">
 			
 			<input type="submit" value="realiza artículo">
 		</fieldset>
