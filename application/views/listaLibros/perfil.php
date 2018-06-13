ca8fc57887c74b369dbae80fae78eb924f5eb9c1
Ethan2001 <usuario@Adriana>
2018-06-13 22:31:58 +0200

    centrar foto response

--- application/views/listaLibros/perfil.php
+++ application/views/listaLibros/perfil.php
@@ -1,10 +1,10 @@
 <div class="container">
 	<div class="header col-md-12">
 		<div class="col-md-12">
-			<div class="col-md-3">
+			<div class="col-md-6">
 				<img class="imagen" src="<?= base_url()?>assets/img/Koala.jpg" alt="koala">
 			</div>
-			<div class="col-md-3" id="informacion">
+			<div class="col-md-4" id="informacion">
 				<label for="lista">Nombre Lista : </label>
 				<br> <span><?= $lista->nombre?></span><br>
 				<br>
