d3a5176daa3bf35dc89e43e3189d4a0de42d48af
Ethan2001 <usuario@Adriana>
2018-06-15 01:13:27 +0200

    response perfil listar Autor

--- application/views/listaLibros/perfil.php
+++ application/views/listaLibros/perfil.php
@@ -2,7 +2,7 @@
 	<div class="header col-md-12">
 		<div class="col-md-12">
 			<div class="col-md-6">
-				<img class="imagen" src="<?= base_url()?>assets/img/listalibros.jpg" alt="koala">
+				<img class="imagen" src="<?= base_url()?>assets/img/listalibros.png" alt="koala">
 			</div>
 			<div class="col-md-4" id="informacion">
 				<label for="lista">Nombre Lista : </label>
@@ -18,7 +18,7 @@
 			</div>
 		</div>
 		 <div class=" biografia col-sm-10 col-md-12">
-			<label for="biografia">Descripción</label><br><br>
+			<label for="biografia">Descripción:</label><br><br>
 	               <div class="expandable">
 	                <p id="biografia" class="letra"><?= $lista->descripcion ?></p><br><br>
 	                </div>
