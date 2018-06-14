74d4c237f8f18a00dd0c5bfa678b5500e438c908
Ethan2001 <usuario@Adriana>
2018-06-14 23:03:41 +0200

    footer y fontello

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,16 @@
 
+<footer class="col-sm-12">
+	<div class="social">
+	<span><i class="icon-facebook"></i></span>
+	<span><i class="icon-instagram"></i></span>
+	</div>
+	
+	<div class="nosotros">
+		<span>@Liber Locus</span>
+	</div>
+
+   
+</footer>
+
 
 
