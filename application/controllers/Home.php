8521d5a81ab9a027b7967a72054f53c03b453540
Juanfro <juanfropro@gmail.com>
2018-06-15 22:29:45 +0200

    Arreglado Listar Articulos con Guest

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,6 +10,10 @@ class Home extends CI_Controller{
 	public function vista(){
 		$this->load->model('articulo_model');
 		$datos['articulos']=$this->articulo_model->getAll();
+		
+		//Datos de Usuario
+		$this->load->model('usuario_model');
+		
 		enmarcar($this, 'home/vista',$datos);
 	}
 	
--- application/views/home/vista.php
+++ application/views/home/vista.php
@@ -1,27 +1,15 @@
 <div class='container'>
 
-<div class="header col-md-12">
-       
-                
-              
-			<!-- limite de articulos mostrados -->
-			
-			<?php $articulos = R::findCollection('articulo','ORDER BY fecha DESC LIMIT 4');
-			  
-			 
-				   while ($articulo = $articulos->next()) {
-				  
-				  echo "<div class='col-md-5 expandable' id='contarticulo'>";
-				   	echo $articulo->contenido;
-				   echo "</div>";   
-				   }?>
-			
-				
-				
-				  
-            
-        
-       
-    </div>
+	<div class="header col-md-12">	
+				<!-- limite de articulos mostrados -->			
+	<?php $articulos = R::findCollection('articulo','ORDER BY fecha DESC LIMIT 4');			  
+		 
+		while ($articulo = $articulos->next()) {  
+			echo "<div class='col-md-5 expandable' id='contarticulo'>";
+				echo $articulo->contenido;
+			echo "</div>";   
+	   }
+	?>
+	</div>
 
 </div>
