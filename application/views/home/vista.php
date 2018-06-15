792c21d34ffb64e1e4b33e29633e7126eb781837
Juanfro <juanfropro@gmail.com>
2018-06-15 22:32:48 +0200

    Refactorización de Vista Home

--- application/views/home/vista.php
+++ application/views/home/vista.php
@@ -2,14 +2,15 @@
 
 	<div class="header col-md-12">	
 				<!-- limite de articulos mostrados -->			
-	<?php $articulos = R::findCollection('articulo','ORDER BY fecha DESC LIMIT 4');			  
+	<?php $articulos = R::findCollection('articulo','ORDER BY fecha DESC LIMIT 4');?>			  
 		 
-		while ($articulo = $articulos->next()) {  
-			echo "<div class='col-md-5 expandable' id='contarticulo'>";
-				echo $articulo->contenido;
-			echo "</div>";   
-	   }
-	?>
+	<?php while($articulo = $articulos->next()):?>
+		 
+		<div class='col-md-5 expandable' id='contarticulo'>
+			<?=$articulo->contenido; ?>
+		</div> 
+	  
+	<?php endwhile;?>
 	</div>
 
 </div>
