5e55b5efeafeb5b234c10386139a2f8115a8688e
Ethan2001 <usuario@Adriana>
2018-06-17 19:37:35 +0200

    boostrap crear usuario

--- application/views/usuario/crear.php
+++ application/views/usuario/crear.php
@@ -1,48 +1,62 @@
 <div class="container">
-	<form class="form form-horizontal" id="idForm" action="<?=base_url()?>usuario/crearPost" method="post">
+	<form class="form-horizontal" id="idForm" action="<?=base_url()?>usuario/crearPost" method="post">
 		<fieldset>
 			<legend>
 				<label for="idusuario"> Usuario</label>
 			</legend>
 			
 			<div class="form-group">
-				<label class="control-label" for="nombre">Nombre</label>
-				<input class="form-control" type="text" name="nombre" id="nombre"></input><br>
-				
-	
-				<label class="control-label" for="apellido">Apellido</label>
-				<input class="form-control" type="text" name="apellido" id="apellido"></input><br>
-				
-				
-				<label class="control-label" for="alias">Alias</label>
-				<input class="form-control" type="text" name="alias" id="alias" required="required"></input><br>
-				
-	
-				<label class="control-label" for="contrasena">Contraseña</label>
-				<input class="form-control" type="password" name="contrasena" id="contrasena" required="required"></input><br>
-				
-				
-				<?php if(isset($_SESSION['usuario']['rol']) && $_SESSION['usuario']['rol']=='administrador'):?>
-					<label class="control-label" for="rol">Rol</label>
-					<!-- <input type="text" name="rol" id="rol" required="required"></input> -->
-					<select name="rol" id="rol">
-						<option value="usuario">Usuario</option>
-						<option value="editor">editor</option>
-						<option value="administrador">Administrador</option>				
-					</select>
-					
-				<?php endif;?>
-				
-				<label class="control-label" for="email">Email</label>
-				<input class="form-control" type="email" name="email" id="email" required="required"></input><br>
-				
-	
-				<label class="control-label" for="edad">Edad</label>
-				<input class="form-control" type="text" name="edad" id="edad" onkeyup="validarEdad()"></input><br>
-				<div id="pasaEdad"></div>
-				
-				<button type="submit" id="botonEnviar" value="registrar usuario">Registrar Usuario</button>				
+				<label class="control-label col-sm-2" for="nombre">Nombre</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="text" name="nombre" id="nombre"></input>
+				</div>
 			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="apellido">Apellido</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="text" name="apellido" id="apellido"></input>
+				</div>
+			</div>	
+			<div class="form-group">	
+				<label class="control-label col-sm-2" for="alias">Alias</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="text" name="alias" id="alias" required="required"></input>
+				</div>
+			</div>	
+	        <div class="form-group">
+				<label class="control-label col-sm-2" for="contrasena">Contraseña</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="password" name="contrasena" id="contrasena" required="required"></input>
+				</div>
+			</div>	
+			<div class="form-group">
+				<?php if(isset($_SESSION['usuario']['rol']) && $_SESSION['usuario']['rol']=='administrador'):?>
+					<label class="control-label col-sm-2 " for="rol">Rol</label>
+					<!-- <input type="text" name="rol" id="rol" required="required"></input> -->
+						<div class="col-sm-4">
+							<select  class="form-control col-sm-4" name="rol" id="rol">
+								<option value="usuario">Usuario</option>
+								<option value="editor">Editor</option>
+								<option value="administrador">Administrador</option>				
+							</select>
+						</div>
+				<?php endif;?>
+			</div>
+			 <div class="form-group">	
+				<label class="control-label col-sm-2" for="email">Email</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="email" name="email" id="email" required="required"></input><br>
+				</div>
+			</div>	
+	   		<div class="form-group">
+				<label class="control-label col-sm-2" for="edad">Edad</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="text" name="edad" id="edad" onkeyup="validarEdad()"></input><br>
+						<div id="pasaEdad"></div>
+			    </div>
+			</div>	    
+				<button class="btn" type="submit" id="botonEnviar" value="registrar usuario">Registrar Usuario</button>				
+			
 		</fieldset>
 	</form>
 
